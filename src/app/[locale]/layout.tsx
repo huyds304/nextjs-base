cc4048d2126e94a30a1608a4cc4e027e8edc1fb3
huydssparkminds <huyds@SparkMinds.net>
2025-07-07 14:33:54 +0700

    remove package

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,4 @@
-import '@/assets/styles/global.scss';
+import '@/assets/styles/global.css';
 
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, setRequestLocale } from 'next-intl/server';
